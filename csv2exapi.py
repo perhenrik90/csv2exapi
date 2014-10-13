48c4a144cc806c68a1a5c4222b6c444d5c751e62
Per-Henrik E Kvalnes <perhenrik.kvalnes@gmail.com>
2014-10-13 19:43:08 +0200

    classifyed the function

--- csv2exapi.py
+++ csv2exapi.py
@@ -3,60 +3,88 @@
 import sys
 import json
 
-infile = open("test.csv")
-outfile = open("out.json", "w")
+class Csv2ExApi:
 
-first = 0
-header = {}
+    inpath = "test.csv"
+    header = {}
+    first = 0
 
-# static headers
-email = "email"
-name = "name"
-code = "code"
-timestamp = "completed"
-codeprefix = "act:campus:"
-verbID = "www.nob-ordbok.uio.no/perl/ordbok.cgi?OPP=fullføre&bokmaal=+&ordbok=bokmaal"
+    # set column names in csv file
+    email = "email"
+    name = "name"
 
-splitValue = ";"
-jsonList = []
+    objectID = "code"
+    objectIDprefix = "act:campus:"
+    timestamp = "completed"
+    verbID = "www.nob-ordbok.uio.no/perl/ordbok.cgi?OPP=fullføre&bokmaal=+&ordbok=bokmaal"
 
-for line in infile:
+    splitValue = ";"
+    jsonList = []
 
-    # if first time, prepare the header 
-    if not first:
-        line = line.strip("\n")
-        keys = line.split(";")
-
-        i = 0
-        for key in keys:
-            if key != "":
-                header[key] = i
-                i += 1
-
-        print ("Headers")
-        print ("-------")
-        print (header)
-        first = 1
-
-    # if noe, make an json object
-    else:
-
-        line = line.strip("\n")
-        values = line.split(splitValue)
+    def __init__(self, splitValue=";"):
+        self.splitValue = splitValue
 
 
-        actorObj = {"mbox":values[header[email]]}
-        verbObj = {"id":verbID}
-        objectObj = {"id":codeprefix+values[header[code]]}
-        paramsObj = {}
-        timestampObj = values[header[timestamp]]
+    def setObjectPrefix(newidPrefix):
+        # set a new prefix for object id's
+        self.objectIDprefix = newidPrefix
 
-        obj = {"actor":actorObj, "verb":verbObj, 
-               "params":paramsObj, "object":objectObj,
-               "timestamp":timestampObj}
-        jsonList.append(obj)
 
-jsonString = json.dumps(jsonList, indent=4)
-print(jsonString)
+
+    def execute(self):        
+        # create json list as string
+        infile = open(self.inpath)
+
+        first = self.first
+        header = self.header
+        objectID = self.objectID
+        objectIDprefix = self.objectIDprefix
+        verbID = self.verbID
+        email = self.email
+        splitValue = self.splitValue
+        jsonList = self.jsonList
+        timestamp = self.timestamp
+
+        for line in infile:
+        # if first time, prepare the header 
+            if not first:
+                line = line.strip("\n")
+                keys = line.split(";")
+
+                i = 0
+                for key in keys:
+                    if key != "":
+                        header[key] = i
+                        i += 1
+
+                print ("Headers")
+                print ("-------")
+                print (header)
+                first = 1
+
+                    
+            else:
+            # if noe, make an json object
+                line = line.strip("\n")
+                values = line.split(splitValue)
+
+
+                actorObj = {"mbox":values[header[email]]}
+                verbObj = {"id":verbID}
+                objectObj = {"id":objectIDprefix+values[header[objectID]]}
+                timestampObj = values[header[timestamp]]
+        
+                obj = {"actor":actorObj, "verb":verbObj, 
+                       "object":objectObj,
+                       "timestamp":timestampObj}
+                jsonList.append(obj)
+                
+        jsonString = json.dumps(jsonList, indent=4)
+        return jsonString
+
+cv = Csv2ExApi()
+str = cv.execute()
+print(str)
+#print(jsonString)
 #outfile.write(jsonString)
 #outfile.close()
